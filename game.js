78c02639a5bb24450e1487f25b533cae47c19008
Kamran Elchuzade <kamranelchuzade@Kamrans-MacBook-Air.local>
2019-01-30 12:30:48 +0300

    testing setup and draw

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
 console.log('game started');
+
+function setup() {
+    createCanvas(1000, 680);
+}
+function draw() {
+    background(0);
+    drawRect();
+}
+
+function drawRect() {
+    fill(100, 20, 210);
+    rect(100, 100, 50, 50);
+}
